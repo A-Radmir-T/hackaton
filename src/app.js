385daa8636671b3a962237715729e1c3a10e9257
A-Radmir-T <131308803+A-Radmir-T@users.noreply.github.com>
2023-07-28 23:47:56 +0500

    Revert "Feature/background module"

--- src/app.js
+++ src/app.js
@@ -1,9 +1,2 @@
 import './styles.css'
-import {ContextMenu} from "@/menu";
-import {BackgroundModule} from "@/modules/background.module";
 
-const modules = [BackgroundModule]
-
-const contextMenu = new ContextMenu('#menu', modules)
-
-contextMenu.init()
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,14 +1,5 @@
 import {Module} from '../core/module'
-import {generateRandomColor} from "@/utils";
 
 export class BackgroundModule extends Module {
-  static dataType = 'backgroundModule'
-  constructor() {
-    super(BackgroundModule.dataType, 'Поменять цвет')
-    this.bodyEl = document.querySelector('body')
-  }
 
-  trigger() {
-    this.bodyEl.style.background = generateRandomColor()
-  }
 }
