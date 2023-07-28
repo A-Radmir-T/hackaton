c51d5f73dc54547f9fd3e676703bc2e9d8cf0090
Fida-VS <poletaeva69vera@mail.ru>
2023-07-29 00:05:45 +0300

    add customMessage in array modules

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import './styles.css'
 import {ContextMenu} from "@/menu";
 import {BackgroundModule} from "@/modules/background.module";
 
-const modules = [BackgroundModule, clickCounterModule]
+
+const modules = [BackgroundModule, clickCounterModule, CustomMessage]
 
 const contextMenu = new ContextMenu('#menu', modules)
 
