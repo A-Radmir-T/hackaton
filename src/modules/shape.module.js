bd03ee0b83965022ce6029da08518a8b4204aa99
Fida-VS <poletaeva69vera@mail.ru>
2023-07-29 20:17:39 +0300

    change shapes

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -13,17 +13,14 @@ export class ShapeModule extends Module {
 
     trigger(){
 
-        const width = random(20, 800);
-        const height = random(30, 600);
+        const width = random(20, 500);
+        const height = random(30, 500);
         const color = generateRandomColor();
-        const borderTopLeftRadius = random(1, 80);
-        const borderTopRighttRadius = random(0, 80);
-        const borderBottomRighttRadius = random(0, 80);
-        const borderBottomLeftRadius = random(0, 80);
-        const left = random(0, 800);
-        const right = random(0, 800);
-        const top = random(0, 800);
-        const bottom = random(0, 800);
+        const borderRadius = random(1, 50);
+        const left = random(0, 600);
+        const right = random(0, 600);
+        const top = random(0, 600);
+        const bottom = random(0, 600);
 
         
 
@@ -32,10 +29,7 @@ export class ShapeModule extends Module {
         randomShape.style.width = `${width}px`;
         randomShape.style.height = `${height}px`;
         randomShape.style.backgroundColor = `${color}`;
-        randomShape.style.borderTopLeftRadius = `${borderTopLeftRadius}%`;
-        randomShape.style.borderTopRighttRadius = `${borderTopRighttRadius}%`;
-        randomShape.style.borderBottomRighttRadius = `${borderBottomRighttRadius}%`;
-        randomShape.style.borderBottomLeftRadius = `${borderBottomLeftRadius}%`;
+        randomShape.style.borderRadius = `${borderRadius}%`;
         randomShape.style.left = `${left}px`;
         randomShape.style.right = `${right}px`;
         randomShape.style.top = `${top}px`;
