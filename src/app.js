1bb34a8fb509e574333268f93f849d26414c5fb6
Fida-VS <poletaeva69vera@mail.ru>
2023-07-29 18:28:40 +0300

    shapeModule init

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ import {ContextMenu} from "@/menu";
 import {BackgroundModule} from "@/modules/background.module";
 import { CustomMessage } from './modules/customMessage.module';
 import {ClickCounter} from "@/modules/clickCounter.module";
+import { ShapeModule } from './modules/shape.module';
 
-const modules = [BackgroundModule, ClickCounter, CustomMessage]
+const modules = [BackgroundModule, ClickCounter, CustomMessage, ShapeModule]
 
 const contextMenu = new ContextMenu('#menu', modules)
 
--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,5 +1,31 @@
-import {Module} from '../core/module'
+import {Module} from '../core/module';
+import {random} from '../utils';
+import { generateRandomColor } from '../utils';
 
 export class ShapeModule extends Module {
+
+    static dataType = 'shapeModule'
+
+    constructor(){
+        super(ShapeModule.dataType, 'Создать фигуру')
+        this.bodyEl = document.querySelector('body')
+    }
+
+    trigger(){
+
+        const width = random(20, 80);
+        const heigth = random(30, 60);
+        const color = generateRandomColor();
+
+        const randomShape = document.createElement('div');
+        this.bodyEl.append(randomShape);
+        randomShape.className = 'random-shape';
+        randomShape.style.width = `${width}vw`;
+        randomShape.style.heigth = `${heigth}vh`;
+        randomShape.style.color = `${color}`;
+
+
+
+    }
 
 }
