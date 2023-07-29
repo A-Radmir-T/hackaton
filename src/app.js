8f08145fd51e2a939ba51c9e2a9b1fe6a24c6bf9
VictorKrylach23 <140835080+VictorKrylach23@users.noreply.github.com>
2023-07-29 10:14:18 -0400

    Update app.js
    add import clickCounter

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ import './styles.css'
 import {ContextMenu} from "@/menu";
 import {BackgroundModule} from "@/modules/background.module";
 import { CustomMessage } from './modules/customMessage.module';
+import {ClickCounter} from "@/modules/clickCounter.module";
 
-const modules = [BackgroundModule, clickCounterModule, CustomMessage]
+const modules = [BackgroundModule, ClickCounter, CustomMessage]
 
 const contextMenu = new ContextMenu('#menu', modules)
 
