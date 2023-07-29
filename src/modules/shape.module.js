b0ec502640d278d216873b3e76d59e259d7513a9
A-Radmir-T <131308803+A-Radmir-T@users.noreply.github.com>
2023-07-29 23:48:50 +0500

    Update shape.module.js

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -9,31 +9,25 @@ export class ShapeModule extends Module {
     this.bodyEl = document.querySelector('body')
     this.shapeEl = document.createElement('div');
     this.shapeEl.className = 'random-shape'
-    this.sapeStyles = {}
+    this.shapeStyles = {}
     this.bodyEl.append(this.shapeEl)
   }
 
   trigger() {
     this.shapeEl.remove()
-    document.onclick = () => {
-      const windowWidth = window.innerWidth - 100
-      const windowHeight = window.innerHeight - 100
-      this.sapeStyles.top = random(0, +windowHeight) + 'px'
-      this.sapeStyles.left = random(0, +windowWidth) + 'px'
-      console.log(this.sapeStyles.top)
-      this.sapeStyles.width = random(40, 100) + 'px'
-      this.sapeStyles.height = random(40, 100) + 'px'
-      this.sapeStyles.borderRadius = random(5, 50) + '%'
-      this.sapeStyles.backgroundColor = generateRandomColor()
+    const windowWidth = window.innerWidth - 100
+    const windowHeight = window.innerHeight - 100
+    this.shapeStyles.top = random(0, +windowHeight) + 'px'
+    this.shapeStyles.left = random(0, +windowWidth) + 'px'
+    this.shapeStyles.width = random(40, 100) + 'px'
+    this.shapeStyles.height = random(40, 100) + 'px'
+    this.shapeStyles.borderRadius = random(5, 50) + '%'
+    this.shapeStyles.backgroundColor = generateRandomColor()
 
-      Object.keys(this.sapeStyles).forEach(key => {
-        this.shapeEl.style[key] = this.sapeStyles[key]
-      })
+    Object.keys(this.shapeStyles).forEach(key => {
+      this.shapeEl.style[key] = this.shapeStyles[key]
+    })
 
-      this.bodyEl.append(this.shapeEl)
-      document.onclick = null
-
-    }
-
+    this.bodyEl.append(this.shapeEl)
   }
 }
