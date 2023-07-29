2d24f4cde64f8d8bcee30e28fcb6d43aeff8990f
Fida-VS <poletaeva69vera@mail.ru>
2023-07-29 19:55:14 +0300

    other changes

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -14,15 +14,34 @@ export class ShapeModule extends Module {
     trigger(){
 
         const width = random(20, 800);
-        const heigth = random(30, 600);
+        const height = random(30, 600);
         const color = generateRandomColor();
+        const borderTopLeftRadius = random(1, 80);
+        const borderTopRighttRadius = random(0, 80);
+        const borderBottomRighttRadius = random(0, 80);
+        const borderBottomLeftRadius = random(0, 80);
+        const left = random(0, 800);
+        const right = random(0, 800);
+        const top = random(0, 800);
+        const bottom = random(0, 800);
+
+        
 
         const randomShape = document.createElement('div');
-        this.bodyEl.append(randomShape);
         randomShape.className = 'random-shape';
         randomShape.style.width = `${width}px`;
-        randomShape.style.heigth = `${heigth}px`;
+        randomShape.style.height = `${height}px`;
         randomShape.style.backgroundColor = `${color}`;
+        randomShape.style.borderTopLeftRadius = `${borderTopLeftRadius}%`;
+        randomShape.style.borderTopRighttRadius = `${borderTopRighttRadius}%`;
+        randomShape.style.borderBottomRighttRadius = `${borderBottomRighttRadius}%`;
+        randomShape.style.borderBottomLeftRadius = `${borderBottomLeftRadius}%`;
+        randomShape.style.left = `${left}px`;
+        randomShape.style.right = `${right}px`;
+        randomShape.style.top = `${top}px`;
+        randomShape.style.bottom = `${bottom}px`;
+
+        this.bodyEl.append(randomShape);
         setTimeout(() => {
             randomShape.remove();
         }, 2000)
