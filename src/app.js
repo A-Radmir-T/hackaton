ce9e115242b7e18c93ab0ff22431b4cc71c8e1bc
Victor Krylach <vkrya2023@gmail.com>
2023-07-29 12:26:49 -0400

    add maps and make style for clicker, just need add script with key at index html

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ import {ContextMenu} from "@/menu";
 import {BackgroundModule} from "@/modules/background.module";
 import { CustomMessage } from './modules/customMessage.module';
 import {ClickCounter} from "@/modules/clickCounter.module";
+import {CurrentLocation} from "@/modules/currentLocation.module";
 
-const modules = [BackgroundModule, ClickCounter, CustomMessage]
+const modules = [BackgroundModule, ClickCounter, CustomMessage, CurrentLocation]
 
 const contextMenu = new ContextMenu('#menu', modules)
 
---
+++ src/modules/currentLocation.module.js
@@ -0,0 +1,54 @@
+import {Module} from '../core/module'
+
+export class CurrentLocation extends Module {
+    static dataType = 'CurrentLocation'
+    constructor() {
+      super(CurrentLocation.dataType, 'Показать мою геолокацию')
+      this.bodyEl = document.querySelector('body')
+    }
+  
+    trigger() {
+        const mapDiv = document.createElement('div')
+        mapDiv.id = 'googleMap'
+        mapDiv.style.height = '90%';
+        mapDiv.style.width = '60%';
+        mapDiv.style.margin = '0 auto'
+        this.bodyEl.append(mapDiv)
+        let map = null;
+    //   document.getElementById('getLocation').addEventListener('click', function() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            let pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            map = new google.maps.Map(mapDiv, {
+              center: pos,
+              zoom: 12
+            });
+
+            let marker = new google.maps.Marker({
+              position: pos,
+              map: map
+            });
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+
+          function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+            infoWindow.setPosition(pos);
+            infoWindow.setContent(browserHasGeolocation ?
+              'Error: The Geolocation service failed.' :
+              'Error: Your browser doesn\'t support geolocation.');
+            infoWindow.open(map);
+          }
+        }
+        setTimeout(() => {
+            mapDiv.remove()
+    }, 10000);
+      }
+    }
