bf9340d0bdf4212328c5c83c27d47c9f7dd01de4
Victor Krylach <vkrya2023@gmail.com>
2023-07-29 12:00:29 -0400

    Current location module

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ import {ContextMenu} from "@/menu";
 import {BackgroundModule} from "@/modules/background.module";
 import { CustomMessage } from './modules/customMessage.module';
 import {ClickCounter} from "@/modules/clickCounter.module";
+import {CurrentLocation} from "@/modules/clickCounter.module";
 
-const modules = [BackgroundModule, ClickCounter, CustomMessage]
+
+const modules = [BackgroundModule, ClickCounter, CustomMessage, CurrentLocation]
 
 const contextMenu = new ContextMenu('#menu', modules)
 
